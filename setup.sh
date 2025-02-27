fc55fe7524165bbdcd11bc7e1c2046eb000acab1
William Hearn <sylus1984@gmail.com>
2025-02-27 10:54:13 -0500

    feat(docs): Minor updates to base

--- setup.sh
+++ setup.sh
@@ -108,16 +108,16 @@ do_helm "${BOOTSTRAP_CLUSTER}" \
   --set mgmt.components.argoFoundation.argocdInstance.notifications.enabled="false" \
   --set "mgmt.components.argoFoundation.argocdProjects.platform.applicationSet.generator.git.repoURL=${CLUSTER_REPOSITORY}" \
   --set "mgmt.components.argoFoundation.argocdProjects.platform.applicationSet.generator.git.revision=main" \
-  --set "mgmt.components.argoFoundation.argocdProjects.platform.applicationSet.generator.git.files[0].path=${CLUSTER_PATH}/${CLUSTER_NAME}/config.yaml" \
+  --set "mgmt.components.argoFoundation.argocdProjects.platform.applicationSet.generator.git.files[0].path=${CLUSTER_PATH}/{CLUSTER_NAME}/**/config.yaml" \
   --set "mgmt.components.argoFoundation.argocdProjects.platform.applicationSet.template.source.repoURL=${HELM_REPOSITORY}" \
   --set "mgmt.components.argoFoundation.argocdProjects.solutions.applicationSet.generator.git.repoURL=${CLUSTER_REPOSITORY}" \
   --set "mgmt.components.argoFoundation.argocdProjects.solutions.applicationSet.generator.git.revision=main" \
-  --set "mgmt.components.argoFoundation.argocdProjects.solutions.applicationSet.generator.git.files[0].path=${CLUSTER_PATH}/${CLUSTER_NAME}/config.yaml" \
+  --set "mgmt.components.argoFoundation.argocdProjects.solutions.applicationSet.generator.git.files[0].path=${NAMESPACE_PATH}/**/config.yaml" \
   --set "mgmt.components.argoFoundation.argocdProjects.solutions.applicationSet.template.source.repoURL=${HELM_REPOSITORY}" \
   --set "mgmt.components.billOfLanding.enabled=false" \
   -f base/argocd-bootstrap.yaml \
   --force \
-  --version 0.0.2 \
+  --version 0.0.4 \
   aurora-platform \
   aurora/aurora-platform
 
