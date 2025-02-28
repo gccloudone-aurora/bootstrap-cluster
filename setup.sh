0a0b791f93e9e8bde339649f41247438d6ff7223
William Hearn <sylus1984@gmail.com>
2025-02-27 20:58:18 -0500

    feat(bootstrap): Adjustments to cluster path

--- setup.sh
+++ setup.sh
@@ -117,7 +117,7 @@ do_helm "${BOOTSTRAP_CLUSTER}" \
   --set "mgmt.components.billOfLanding.enabled=false" \
   -f base/argocd-bootstrap.yaml \
   --force \
-  --version 0.0.6 \
+  --version 0.0.7 \
   aurora-platform \
   aurora/aurora-platform
 
